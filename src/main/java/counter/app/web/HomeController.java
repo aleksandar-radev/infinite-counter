00246a5271b191677113da24ec5b3e8839baef10
duhcheto96 <alexander.radev.96@gmail.com>
2021-02-27 01:45:05 +0200

    test

--- src/main/java/counter/app/web/HomeController.java
+++ src/main/java/counter/app/web/HomeController.java
@@ -3,7 +3,7 @@ package counter.app.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
