60a04cd9d0dd9386f2f22913891b0fc473a685a2
duhcheto96 <alexander.radev.96@gmail.com>
2021-02-27 01:36:30 +0200

    test

--- src/main/java/counter/app/web/HomeController.java
+++ src/main/java/counter/app/web/HomeController.java
@@ -1,11 +1,7 @@
-package counter.web;
+package counter.app.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HomeController {
