30e0492ce361340cbf8f69ad866468dc4e627669
duhcheto96 <alexander.radev.96@gmail.com>
2021-02-26 14:51:48 +0200

    spring project created

---
+++ demo/src/test/java/counter/demo/CounterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package counter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
